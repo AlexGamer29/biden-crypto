2da8b47bd15ae5239f008d4bcc1f29c2a622b437
nguyentr02 <nguyen.truong200226@vnuk.edu.vn>
2022-04-05 12:25:57 +0700

    Change source

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
+import '../css/home.css';
 
 export const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <div className="leftSide">
+
+      </div>
+      <div className="rightSide">
+
+      </div>
     </div>
   );
 };
