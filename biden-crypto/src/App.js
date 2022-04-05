8fd8e59be3237db4d7ee51747c67db66f89c98e3
DNDAHieuBeo <90563964+DNDAHieuBeo@users.noreply.github.com>
2022-04-05 11:37:44 +0700

    Bug-ne-tk-lz

--- biden-crypto/src/App.js
+++ biden-crypto/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./component/navbar";
+import { Router, Route } from "react-router-dom";
+import { Home } from "./page/home";
+import { About } from "./page/about";
+import { Product } from "./page/product";
+import { Contact } from "./page/contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Router>
+        <div className="pages">
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/contact" element={<Contact />} />
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ biden-crypto/src/page/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../component/navbar";
+
+export const Home  = () => {
+  return (
+    <div>
+        <Navbar/>
+      <h1>Blog</h1>
+    </div>
+  );
+};
