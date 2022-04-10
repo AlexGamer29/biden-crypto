8781b9125715b9b988e6aa03312e9bc9c4f445a1
nguyentr02 <nguyen.truong200226@vnuk.edu.vn>
2022-04-10 10:07:34 +0700

    [add] add Home-page content
    Briefly add second content at the bottom of the page.

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -5,15 +5,37 @@ export const Home = () => {
   return (
     <div className="Body">
       <div className="content1">
-        <div className="leftSide">
-          <h1>Buy, trade, and hodl 100+ coins on Biden crypto</h1>
-            <button name="button" type="button" id="bt"
+        <div className="leftSide1">
+          <h1>Buy, trade, and hodl 100+ coins!</h1>
+          <h4>Biden-crypto is the easiest place to buy and sell cryptocurrency. Let's start your journey.</h4>
+            {/* <button name="button" type="button" id="bt"
             >Get started
-            </button>
+            </button> */}
         </div>
-        <div className="rightSide">
+        <div className="rightSide1">
           <img id="body-img" src="https://media4.giphy.com/media/JpttR9RXNbTwoT7jAz/giphy.gif?cid=6c09b952fmp52q6kaxv6olp195pk4k4q4v3vky6jz4yibmnx&rid=giphy.gif&ct=s" alt="IMG">
           </img>
+        </div>
+      </div>
+      <div className="content2">
+        <h1 id="ct2_h1">Create your wonderful portfolio today</h1>
+        <h6 id="ct2_h6">Biden-crypto has a variety of features that make it the most perfect place to start trading</h6>
+
+        <div className="in_content2">
+            <div className="leftSide2">
+                <div className="box1">
+                    {/* <img alt="img" src="https://png.pngtree.com/png-vector/20190120/ourlarge/pngtree-graph-vector-icon-png-image_470654.jpg"></img> */}
+                </div>
+                <div className="box2">
+
+                </div>
+                <div className="box3">
+
+                </div>
+            </div>
+            <div className="rightSide2">
+
+            </div>
         </div>
       </div>
     </div>
