968429a6d681d17f409f6037ffcb86fba1d5db3c
nguyentr02 <nguyen.truong200226@vnuk.edu.vn>
2022-04-16 12:24:00 +0700

    (update) add content for home page
    Add some imgs and texts for the downside of home page
    As well as finishing the very first stage of the home page

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -8,7 +8,7 @@ export const Home = () => {
         <div className="leftSide1">
           <h1>Buy, trade, and hodl 100+ coins!</h1>
           <h4>Biden-crypto is the easiest place to buy and sell cryptocurrency. Let's start your journey.</h4>
-            {/* <button name="button" type="button" id="bt"
+          {/* <button name="button" type="button" id="bt"
             >Get started
             </button> */}
         </div>
@@ -22,20 +22,41 @@ export const Home = () => {
         <h6 id="ct2_h6">Biden-crypto has a variety of features that make it the most perfect place to start trading</h6>
 
         <div className="in_content2">
-            <div className="leftSide2">
-                <div className="box1">
-                    {/* <img alt="img" src="https://png.pngtree.com/png-vector/20190120/ourlarge/pngtree-graph-vector-icon-png-image_470654.jpg"></img> */}
-                </div>
-                <div className="box2">
-
-                </div>
-                <div className="box3">
-
-                </div>
+          <div className="leftSide2">
+            <div className="box1">
+              <div className="img-box1">
+                <img id="img-box1" src="https://i.pinimg.com/564x/7c/46/d1/7c46d10a7ddd73cd773c44531ccc43a5.jpg" alt="img"></img>
+              </div>
+              <div className="text-box1">
+                <h1>Manage your portfolio</h1>
+                <h6> Buy and sell digital currencies, keep track in one place only.</h6>
+              </div>
             </div>
-            <div className="rightSide2">
-
+            <div className="box2">
+              <div className="img-box2">
+                <img id="img-box2" src="https://i.pinimg.com/564x/de/a1/f1/dea1f13f38fd6b28d6fd271f901de6e8.jpg" alt="img"></img>
+              </div>
+              <div className="text-box2">
+                <h1>Recurring buys</h1>
+                <h6>Invest in cryptocurrency slowly over time by scheduling buys daily, weekly, or monthly.</h6>
+              </div>
             </div>
+            <div className="box3">
+              <div className="img-box3">
+                <img id="img-box3" src="https://i.pinimg.com/564x/77/89/58/77895880051c5155e7f2d203cfa5a9db.jpg" alt="img"></img>
+              </div>
+              <div className="text-box3">
+                <h1>Mobile apps</h1>
+                <h6>Coming soon!</h6>
+              </div>
+            </div>
+          </div>
+          <div className="space">
+            {/* Just space, dont touch */}
+          </div>
+          <div className="rightSide2">
+            <img id="img-rightSide2" src="https://i.pinimg.com/564x/38/d6/32/38d632ac63ac22d3ee285a67f71be9cf.jpg" alt="img"></img>
+          </div>
         </div>
       </div>
     </div>
