ca6013d76fe8fcb9aff7e08d47a73140318fca8c
DESKTOP-8FVROPO\ADMIN <letrihieudnda@gmail.com>
2022-04-17 22:56:02 +0700

    create-product-page

--- src/components/Pages/Product.js
+++ src/components/Pages/Product.js
@@ -1,9 +1,26 @@
 import React from "react";
+import coin_card from "../../data/coin_data";
+import '../css/product.css'
 
 export const Product= () => {
+  const listItems = coin_card.map((item)=>
+  <div className="card" key={item.id}>
+    <div className="card_img">
+      <img src={item.img} alt=""/>
+    </div>
+    <div className="card_header">
+      <h2>{item.coin_name}</h2>
+      <h2>{item.description}</h2>
+      <p className="price">{item.currency}<span>{item.price}</span></p>
+      <button className="btn_add">Add to cart</button>
+    </div>
+
+  </div>
+  
+  )
   return (
-    <div>
-      <h1>Blog</h1>
+    <div className="main_contain">
+      {listItems}
     </div>
   );
 };
---
+++ src/data/coin_data.js
@@ -0,0 +1,49 @@
+import bitcoin from './img/bitcoin.png'
+import bnb from './img/bnb.png'
+import etherium from './img/etherium.png'
+
+const coin_card = [
+  {
+    id: 1,
+    coin_name: "Bitcoin",
+    description: "",
+    price: 39976,
+    currency: "$",
+    img: bitcoin,
+  },
+  {
+    id: 2,
+    coin_name: "BNB",
+    description: "",
+    price: 412,
+    currency: "$",
+    img: bnb,
+  },
+  {
+    id: 3,
+    coin_name: "Etherium",
+    description: "",
+    price: 3015,
+    currency: "$",
+    img: etherium,
+  },
+  {
+    id: 4,
+    coin_name: "Etherium",
+    description: "",
+    price: 3015,
+    currency: "$",
+    img: etherium,
+  },
+  {
+    id: 5,
+    coin_name: "Etherium",
+    description: "",
+    price: 3015,
+    currency: "$",
+    img: etherium,
+  },
+  
+
+];
+export default coin_card;
