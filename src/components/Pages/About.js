991573daad2b50eac543bb5c378deee4bc25780b
Thanh <thanh.tran200212@vnuk.edu.vn>
2022-04-19 22:07:13 +0700

    [add] add sections 1 for About page

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,10 +1,27 @@
 import React from "react";
-
+import '../css/about.css';
 export const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p> WHO'RE WE? </p>
+    <div class="page">
+      <div class="content1">
+        <h1>ABOUT</h1>
+
+    
+      </div>  
+      <div class="leftside"> 
+        <img id="body-img" src="https://i.pinimg.com/564x/0d/7d/52/0d7d520cf0764121c6568b070132db11.jpg" alt="IMG"></img>
+      
+      </div> 
+      <div class="content2"> 
+        <h2>THE BIDEN-CRYPTO</h2> 
+        <p>The Biden-Crypto is a is a place to exchange information, capitalization, and prices of the cryptocurrency market.</p>
+
+      </div>
+        
+
+      
+      
     </div>
+
   );
 };
