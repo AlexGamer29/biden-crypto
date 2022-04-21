6c8bb0bd78de2591f8a3fc827653cbad6a942b5e
nguyentr02 <nguyen.truong200226@vnuk.edu.vn>
2022-04-21 16:12:29 +0700

    (edit) edit About.js and About.css
    Edit completely new About page

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -2,25 +2,45 @@ import React from "react";
 import '../css/about.css';
 export const About = () => {
   return (
-    <div class="page">
-      <div class="content1">
-        <h1>ABOUT</h1>
+    <div className="page">
+      <h1>ABOUT</h1>
 
-    
-      </div>  
-      <div class="leftside"> 
-        <img id="body-img" src="https://i.pinimg.com/564x/0d/7d/52/0d7d520cf0764121c6568b070132db11.jpg" alt="IMG"></img>
-      
-      </div> 
-      <div class="content2"> 
-        <h2>THE BIDEN-CRYPTO</h2> 
-        <p>The Biden-Crypto is a is a place to exchange information, capitalization, and prices of the cryptocurrency market.</p>
+      <div className="content1">
+        <div className="leftSide1">
+          <img id="body-img" src="https://i.pinimg.com/564x/0d/7d/52/0d7d520cf0764121c6568b070132db11.jpg" alt="IMG"></img>
+        </div>
+        <div className="rightSide1">
+          {/* text */}
+        </div>
+      </div>
+
+
+      <div className="content2">
+        <h2>Intributor</h2>
+        <div className="img-container-content2">
+          <div className="intributor-container" id="Nguyen">
+            <img src="" alt="img"></img>
+            <p>Truong Pham Tuan Nguyen</p>
+          </div>
+          <div className="intributor-container" id="Duc">
+            <img src="" alt="img"></img>
+            <p>Nguyen Huu Minh Duc</p>
+          </div>
+          <div className="intributor-container" id="Hieu">
+            <img src="" alt="img"></img>
+            <p>Le Tri Hieu</p>
+          </div>
+          <div className="intributor-container" id="Thanh">
+            <img src="" alt="img"></img>
+            <p>Tran Cong Thanh</p>
+          </div>
+        </div>
 
       </div>
-        
 
-      
-      
+
+
+
     </div>
 
   );
