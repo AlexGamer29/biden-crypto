b5170ced6b8e2b8dddb33672fd10b63d79af5e93
Thanh <thanh.tran200212@vnuk.edu.vn>
2022-04-23 12:44:28 +0700

    (edit) edit content1 and 2 in About.js and about.css [add] add section3  in About.js

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,16 +1,21 @@
 import React from "react";
 import '../css/about.css';
+
 export const About = () => {
   return (
     <div className="page">
-      <h1>ABOUT</h1>
+      <h1>ABOUT US</h1>
 
       <div className="content1">
         <div className="leftSide1">
           <img id="body-img" src="https://i.pinimg.com/564x/0d/7d/52/0d7d520cf0764121c6568b070132db11.jpg" alt="IMG"></img>
         </div>
         <div className="rightSide1">
-          {/* text */}
+          <h2>The BINDEN CRYPTO COIN MARKET</h2>
+          <p>  
+      The Biden-Crypto is a place to exchange information, capitalization, and prices of the cryptocurrency market. It's is a decentralized cryptocurrency exchange platform that allows users to trade cryptocurrencies with each other.
+      Biden-crypto is the easiest place to buy and sell cryptocurrency. Let's start your journey.
+          </p>
         </div>
       </div>
 
@@ -19,32 +24,38 @@ export const About = () => {
         <h2>Intributor</h2>
         <div className="img-container-content2">
           <div className="intributor-container" id="Nguyen">
-            <img src="" alt="img"></img>
+            <img src="../../src/assets/img/NGuyen.jpg" alt="img"></img>
             <p>Truong Pham Tuan Nguyen</p>
           </div>
           <div className="intributor-container" id="Duc">
-            <img src="" alt="img"></img>
+            <img src="../../src/assets/img/Duc.png" alt="img"></img>
             <p>Nguyen Huu Minh Duc</p>
           </div>
           <div className="intributor-container" id="Hieu">
-            <img src="" alt="img"></img>
+            <img src="../../src/assets/img/trihieu.jpg" alt="img"></img>
             <p>Le Tri Hieu</p>
           </div>
           <div className="intributor-container" id="Thanh">
-            <img src="" alt="img"></img>
+            <img src="../../src/assets/img/Thanh.jpg" alt="img"></img>
             <p>Tran Cong Thanh</p>
-          </div>
+          </div>  
         </div>
+
       </div>
-
-      <div className="content3">
-        
+      <div className="partners">
+        <h2>Some of our</h2>
+        <h1>PARTNERS</h1>
+        <div className="partners-container">
+          <div className="partners-item">
+            <img src="https://i.pinimg.com/564x/5c/b7/43/5cb74367552116dac922a5a46eb22c63.jpg" alt="img"></img>
+          </div>
+        </div>    
       </div>
-
-
+      
 
 
     </div>
+
 
   );
 };
